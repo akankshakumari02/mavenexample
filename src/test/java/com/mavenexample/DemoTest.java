439e9e03d813a3c613e064adfbbe74a266188719
Akanksha Kumari <aakanksha021997@gmail.com>
2021-02-25 13:32:22 +0530

    Initial commit

---
+++ src/test/java/com/mavenexample/DemoTest.java
@@ -0,0 +1,13 @@
+package com.mavenexample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest
+{
+    @Test
+
+    public void setup()
+    {
+        System.out.println("Hello from Maven test class");
+    }
+}
